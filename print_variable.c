4a37788461deb456b0ac54a13692da8373d9624a
yunnuy0328 <90439843+yunnuy0328@users.noreply.github.com>
2021-09-17 13:49:36 +0800

    Add files via upload

---
+++ print_variable.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    int b = 87;
+    for (int j = 5; j < 23; j++)
+       printf("j=%d, b=%d\n", j, b*j);
+       //j=5, b=435
+        //j=6, b=522
+        //j=7, b=609
+        //j=8, b=696
+        //j=9, b=783
+        //...
+        //j=22, b=1914
+    }
